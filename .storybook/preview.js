19cc9c551eb07950b13a240cbfc5e23741287586
jdwjdwjdw <jacobdeanwilson@gmail.com>
2021-02-02 02:54:47 -0800

    Add storyshots and use to run snapshot tests for stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
+import '../src/index.css'; // The app's CSS file goes here
 
+// Configures Storybook to log the actions ( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
